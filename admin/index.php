d1db45a0211739573aa3cf41f94e82bc2008e1a2
Ameyalli <ameyanezcmm@gmail.com>
2021-05-20 12:28:23 -0500

    Traer resultados de la base de datos

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,69 @@
 <?php
+    //importar la conexion
 
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    //query
+    $query = "SELECT * FROM propiedades";
+    //consultar la BD
+
+    $resultadoConsulta = mysqli_query($db,$query);
+
+    //Muestra mensjae condicional
     $resultado = $_GET['resultado'] ?? null;
 
+    // Incluye un template
     require '../includes/funciones.php';
     includeTemplate('header');
 ?>
 
-    <maiin class="contenedor seccion">
-        <h1>Administrador</h1>
-        <a href="propiedades/crear.php"  class="boton boton-verde">Nueva propiedad</a>
+    <main class="contenedor seccion">
+        <h1>Administrador</h1>  
         <?php 
         if( intval($resultado) === 1):   ?>
             <p class="alerta exito">Anuncio creado correctamente</p>
         <?php endif; ?>
-            </main>
+        <a href="propiedades/crear.php"  class="boton boton-verde">Nueva propiedad</a>
+      
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody> <!--Consultar resultados -->
+                <?php 
+                    while( $propiedad = mysqli_fetch_assoc($resultadoConsulta)):
+                ?>
+                    <tr>
+                        <td>
+                        <?php
+                         echo $propiedad['id'];
+                        ?></td>
+                        <td><?php
+                         echo $propiedad['titulo'];
+                        ?></td>
+                        <td> <img src="../imagenes\<?php echo $propiedad['imagen'];?>" class="imagen-tabla" alt="imagen"> </td>
+                        <td>$ <?php
+                         echo $propiedad['precio'];
+                        ?></td>
+                        <td>
+                            <a href="#" class="boton-rojo-block">Eliminar</a>
+                            <a href="#" class="boton-amarillo-block">Actualizar</a>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+    </main>
 
 <?php
+    // cerrar la conexión
+    mysqli_close($db);
     includeTemplate('footer');
 ?>
